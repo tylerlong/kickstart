40ae3c09b86eb723447582e242d46ef4672db79e
Tyler Long <tyler4long@gmail.com>
2018-01-07 15:32:14 +0800

    Test binary files

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,14 @@
 /* eslint-env jest */
-describe('kickstart', () => {
-  test('binary files', () => {
-    expect(1 + 1).toBe(2)
+const fs = require('fs')
+const path = require('path')
+
+describe('binary files', () => {
+  const logoPath = path.join(__dirname, 'new-project', 'assets', 'logo.png')
+  test('should be copied to target directory', () => {
+    expect(fs.existsSync(logoPath)).toBe(true)
+  })
+  test('should be the same as source file', () => {
+    const sourcePath = path.join(__dirname, 'boilerplate-project', 'assets', 'logo.png')
+    expect(fs.statSync(logoPath).size).toBe(fs.statSync(sourcePath).size)
   })
 })

05339e5f0371954a1550dea46124f50d97866844
Tyler Long <tyler4long@gmail.com>
2018-01-07 19:41:50 +0800

    Test static files

--- test/index.spec.js
+++ test/index.spec.js
@@ -32,3 +32,17 @@ describe('binary file', () => {
     })(sourceFiles)
   })
 })
+
+describe('static file', () => {
+  test('should equal source', () => {
+    R.forEach(filename => {
+      if (!isBinaryFile.sync(path.join(sourceDir, filename))) {
+        const sourceContent = fs.readFileSync(path.join(sourceDir, filename), 'utf-8')
+        if (sourceContent.indexOf('{{') === -1 && sourceContent.indexOf('{%') === -1) {
+          const targetContent = fs.readFileSync(path.join(targetDir, filename), 'utf-8')
+          expect(targetContent).toEqual(sourceContent)
+        }
+      }
+    })(sourceFiles)
+  })
+})
